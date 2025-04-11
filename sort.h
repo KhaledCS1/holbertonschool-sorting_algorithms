7318abd74a77e712f675326646c7805873b5d9ba
KhaledCS1 <10688@holbertonstudents.com>
2025-04-11 19:17:00 +0000

    Added Tasks

--- sort.h
+++ sort.h
@@ -31,4 +31,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void heapify(int *array, size_t size, size_t root, size_t heap_size);
 #endif/* _SORT_H_ */
